320ae20877a1d519f77a1c1ad7a9751a0b1807ad
docker <docker@zergut2012.com>
2014-11-19 02:05:16 +0000

    updated-docker-in-docker

--- docker-in-docker/build.sh
+++ docker-in-docker/build.sh
@@ -1,1 +1,7 @@
 docker build -t clouderg/dubuntu14 .
+
+# to use
+
+#docker run --privileged -t -i clouderg/dubuntu14			#log to stdout
+#docker run --privileged -t -i -e LOG=file clouderg/dubuntu14		#log to file /var/log/docker.log
+#docker run --privileged -d -p 4444 -e PORT=4444 clouderg/dubuntu14	#Run Docker-in-Docker and expose the inside Docker to the outside world
